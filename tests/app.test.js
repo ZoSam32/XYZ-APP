792800a0e7b6faa8b10f4c12540023cf01b957e1
ZoSam32 <lsamaniegojr@gmail.com>
2023-06-01 21:41:08 -0500

    condition reverted and test reorg

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,12 +1,12 @@
 const request = require('supertest')
-const app = require('./src/app')
+const app = require('../src/app')
 
 describe("Test the root path", () => {
     test("It should respond the GET method", done => {
         request(app)
         .get('/')
         .then(response => {
-            expect(response.statusCode).toBe(2001)
+            expect(response.statusCode).toBe(200)
             done()
         })
     })

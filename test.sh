72749a853debee55a7d9f72abc01e6d06f5bb7dd
ZoSam32 <lsamaniegojr@gmail.com>
2023-06-15 20:44:42 -0500

    deploy smoke test

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SAMPLE=`kubectl get svc -n xyz-app --output jsonpath="{.items[0].status.loadBalancer.ingress[0].ip}"`
+
+sleep 10
+
+STATUS=`curl --silent --head $SAMPLE/ready | head -1 | cut -f 2 -d' '`
+
+if [ "$STATUS" != "200" ] 
+then
+    echo "FAILURE, instance not ready"
+    exit 1
+else
+    echo "SUCCESS"
+fi
